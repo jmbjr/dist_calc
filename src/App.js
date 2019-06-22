5b7cafcca6dd57485267225bb37ee250530b56fb
John Boyle <boylejm@gmail.com>
2019-06-22 03:01:26 -0700

    try to do some refactoring...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//import {  StyleSheet } from "react-native";
 import Svg, { Line } from "react-native-svg";
 import "./App.css";
 
@@ -11,9 +10,7 @@ class App extends Component {
       x2: 200,
       y1: 0,
       y2: 0,
-      trueDist: 0,
-      quickDist: 0,
-      iterations: 50000
+      iter: 50000
     };
   }
 
@@ -22,70 +19,89 @@ class App extends Component {
     var dim = e.getBoundingClientRect();
     var x = evt.clientX - dim.left;
     var y = evt.clientY - dim.top;
-    //alert("x: " + x + " y:" + y);
     this.setState({ x2: x, y2: y });
-    //console.log(`Setting x2 to ${x} and y2 to ${y}`);
-    //this.computeDistances(this.state.x1, x, this.state.y1, y);
   };
 
   handleChange = event => {
-    // console.log(
-    //   `Setting State for ${[event.target.name]} to ${Number(
-    //     event.target.value
-    //   )}`
-    // );
     this.setState({
       [event.target.name]: Number(event.target.value)
     });
   };
 
   render() {
-    const { x1, x2, y1, y2, iterations } = this.state;
-    //console.log("Rendering!!!");
+    const { x1, x2, y1, y2, iter } = this.state;
     return (
       <div className="App">
-        <InputBox value={x1} name="x1" handleChange={this.handleChange}>
-          x1
-        </InputBox>
-        <InputBox value={y1} name="y1" handleChange={this.handleChange}>
-          y1
-        </InputBox>
-        <InputBox value={x2} name="x2" handleChange={this.handleChange}>
-          x2
-        </InputBox>
-        <InputBox value={y2} name="y2" handleChange={this.handleChange}>
-          y2
-        </InputBox>
-        <InputBox
-          value={iterations}
-          name="iterations"
+        <Inputs
+          x1={x1}
+          x2={x2}
+          y1={y1}
+          y2={y2}
+          iter={iter}
           handleChange={this.handleChange}
-        >
-          iter
-        </InputBox>
-        <OutputBox
-          value={multiRun(trueLineDist, x1, x2, y1, y2, iterations)}
-          name="sqrtDist"
-        >
-          sqrt
-        </OutputBox>
-        <OutputBox
-          value={multiRun(quickLineDist, x1, x2, y1, y2, iterations)}
-          name="quickDist"
-        >
-          fast
-        </OutputBox>
-        <OutputBox
-          value={multiRun(trueLineDist_exponent, x1, x2, y1, y2, iterations)}
-          name="0.5 exponent Dist"
-        >
-          ^0.5
-        </OutputBox>
+        />
+        <Outputs
+          x1={x1}
+          x2={x2}
+          y1={y1}
+          y2={y2}
+          iter={iter}
+          handleChange={this.handleChange}
+        />
         <SvgLine x1={x1} y1={y1} x2={x2} y2={y2} onClick={this.clickedSVG} />{" "}
       </div>
     );
   }
 }
+
+//can I use map here to simplify this and remove the duplication?
+const Inputs = ({ x1, x2, y1, y2, iter, handleChange }) => {
+  return (
+    <React.Fragment>
+      <InputBox value={x1} name="x1" handleChange={handleChange}>
+        x1
+      </InputBox>
+      <InputBox value={y1} name="y1" handleChange={handleChange}>
+        y1
+      </InputBox>
+      <InputBox value={x2} name="x2" handleChange={handleChange}>
+        x2
+      </InputBox>
+      <InputBox value={y2} name="y2" handleChange={handleChange}>
+        y2
+      </InputBox>
+      <InputBox value={iter} name="iter" handleChange={handleChange}>
+        iter
+      </InputBox>
+    </React.Fragment>
+  );
+};
+
+const Outputs = ({ x1, x2, y1, y2, iter }) => {
+  return (
+    <React.Fragment>
+      <OutputBox
+        value={multiRun(trueLineDist, x1, x2, y1, y2, iter)}
+        name="sqrtDist"
+      >
+        sqrt
+      </OutputBox>
+      <OutputBox
+        value={multiRun(quickLineDist, x1, x2, y1, y2, iter)}
+        name="quickDist"
+      >
+        fast
+      </OutputBox>
+      <OutputBox
+        value={multiRun(trueLineDist_exponent, x1, x2, y1, y2, iter)}
+        name="0.5 exponent Dist"
+      >
+        ^0.5
+      </OutputBox>
+    </React.Fragment>
+  );
+};
+
 const OutputBox = ({ value, name, children }) => {
   return (
     <div>
@@ -102,7 +118,7 @@ const OutputBox = ({ value, name, children }) => {
   );
 };
 
-const InputBox = ({ value, name, style, handleChange, children }) => {
+const InputBox = ({ value, name, handleChange, children }) => {
   return (
     <div>
       {children}
@@ -125,12 +141,11 @@ const SvgLine = ({ x1, x2, y1, y2, onClick }) => {
   );
 };
 
-//ToDo make this one function and pass my functions....
-const multiRun = function(func, x1, y1, x2, y2, iterations) {
+const multiRun = function(func, x1, y1, x2, y2, iter) {
   const t0 = performance.now();
   var ii = 0;
   var retval = 0;
-  for (ii = 0; ii < iterations; ii++) {
+  for (ii = 0; ii < iter; ii++) {
     retval = func(x1, y1, x2, y2).toFixed(2);
   }
   const t1 = performance.now();
